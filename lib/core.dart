ebe008a08d58b7ad3a7f1f2ebda370c4fc8aa93e
efrizalnf <ondabuzz9@gmail.com>
2023-03-09 22:38:41 +0700

    added assets and color util

--- lib/core.dart
+++ lib/core.dart
@@ -5,3 +5,6 @@ it means you have to rename it to something more unique.
 */
 export 'package:flutter_slicing_project/module/home/controller/home_controller.dart';
 export 'package:flutter_slicing_project/module/home/view/home_view.dart';
+export 'package:flutter_slicing_project/module/signup/controller/signup_controller.dart';
+export 'package:flutter_slicing_project/module/signup/view/signup_view.dart';
+export 'state_util.dart';
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-
-import 'module/home/view/home_view.dart';
+import 'package:flutter_slicing_project/module/signup/view/signup_view.dart';
+import 'package:flutter_slicing_project/utils/color_lib.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,10 +12,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primarySwatch: Colors.green,
+        primarySwatch: ColorLib.primaryColor,
       ),
-      home: const HomeView(),
+      home: const SignupView(),
     );
   }
 }
