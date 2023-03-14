deeb2222fdb2d770347dbe7e1a7f8f4cd04ba646
fadlydestriana <fadlydestriana@gmail.com>
2023-03-15 05:49:02 +0700

    done slicing ELogin2

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:flutter_slicing_project/module/login/view/login_view.dart';
+import 'package:flutter_slicing_project/module/login/view/login_view2.dart';
 import 'package:flutter_slicing_project/state_util.dart';
 
 import 'package:flutter/material.dart';
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.green)
             .copyWith(background: Colors.white),
       ),
-      home: const LoginView(),
+      home: const LoginView2(),
       navigatorKey: Get.navigatorKey,
     );
   }
---
+++ lib/module/login/widget/elogin2/grid/egrid_widget2.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_slicing_project/core.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class EGridWidget2 extends StatelessWidget {
+  const EGridWidget2({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.only(left: Get.width * 0.08, right: Get.width * 0.08),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Image.asset(
+            "assets/images/logo.png",
+            width: 69.0,
+            height: 69.0,
+            fit: BoxFit.fill,
+          ),
+          SizedBox(
+            height: Get.height * 0.01,
+          ),
+          Text(
+            'Scheme\niOS Wireframe\nLayouts',
+            style: GoogleFonts.roboto(
+              color: const Color(0xFF455154),
+              fontWeight: FontWeight.w700,
+              fontSize: 36.0,
+            ),
+          ),
+          SizedBox(
+            height: Get.height * 0.02,
+          ),
+          Text(
+            "Biggest collection of 300+ layouts\nfor iOS prototyping.",
+            style: GoogleFonts.roboto(
+              color: const Color(0xFF455154),
+              fontWeight: FontWeight.w400,
+              fontSize: 16.0,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
