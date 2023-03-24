3c02c8774441ffe3ef374d398a62bfd637fcc350
fadlydestriana <fadlydestriana@gmail.com>
2023-03-24 07:45:50 +0700

    editing ELogin3 adding widget test for ELogin3

--- lib/module/login/view/login_view3.dart
+++ lib/module/login/view/login_view3.dart
@@ -11,73 +11,75 @@ class LoginView3 extends StatefulWidget {
 
     return Scaffold(
       body: SafeArea(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            SizedBox(
-              height: Get.height * 0.05,
-            ),
-            Image.asset(
-              "assets/images/logo2.png",
-              width: 112.0,
-              height: 113.0,
-              fit: BoxFit.fill,
-            ),
-            SizedBox(
-              height: Get.height * 0.02,
-            ),
-            Text(
-              "Biggest collection of 300+ layouts\nfor iOS prototyping.",
-              style: GoogleFonts.roboto(
-                  color: const Color(0xFF455154),
-                  fontSize: 18,
-                  fontWeight: FontWeight.w400),
-              textAlign: TextAlign.center,
-            ),
-            SizedBox(
-              height: Get.height * 0.28,
-            ),
-            Text(
-              "Login with social networks",
-              style: GoogleFonts.roboto(
-                  color: const Color(0xFF969FA2),
-                  fontSize: 14,
-                  fontWeight: FontWeight.w400),
-            ),
-            SizedBox(
-              height: Get.height * 0.01,
-            ),
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: Get.width * 0.04),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  const ELoginButton(
-                      label: 'Facebook', icon: Ionicons.logo_facebook),
-                  SizedBox(
-                    width: Get.width * 0.04,
-                  ),
-                  const ELoginButton(
-                      label: 'Twitter', icon: Ionicons.logo_twitter)
-                ],
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              SizedBox(
+                height: Get.height * 0.05,
               ),
-            ),
-            SizedBox(
-              height: Get.height * 0.03,
-            ),
-            Text(
-              "or sign up with Email",
-              style: GoogleFonts.roboto(
-                  color: const Color(0xFF969FA2),
-                  fontSize: 14,
-                  fontWeight: FontWeight.w400),
-            ),
-            const SignupButton(),
-            SizedBox(
-              height: Get.height * 0.08,
-            ),
-            const ETextButton()
-          ],
+              Image.asset(
+                "assets/images/logo2.png",
+                width: 112.0,
+                height: 113.0,
+                fit: BoxFit.fill,
+              ),
+              SizedBox(
+                height: Get.height * 0.02,
+              ),
+              Text(
+                "Biggest collection of 300+ layouts\nfor iOS prototyping.",
+                style: GoogleFonts.roboto(
+                    color: const Color(0xFF455154),
+                    fontSize: 18,
+                    fontWeight: FontWeight.w400),
+                textAlign: TextAlign.center,
+              ),
+              SizedBox(
+                height: Get.height * 0.28,
+              ),
+              Text(
+                "Login with social networks",
+                style: GoogleFonts.roboto(
+                    color: const Color(0xFF969FA2),
+                    fontSize: 14,
+                    fontWeight: FontWeight.w400),
+              ),
+              SizedBox(
+                height: Get.height * 0.01,
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: Get.width * 0.04),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    const ELoginButton(
+                        label: 'Facebook', icon: Ionicons.logo_facebook),
+                    SizedBox(
+                      width: Get.width * 0.04,
+                    ),
+                    const ELoginButton(
+                        label: 'Twitter', icon: Ionicons.logo_twitter)
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: Get.height * 0.03,
+              ),
+              Text(
+                "or sign up with Email",
+                style: GoogleFonts.roboto(
+                    color: const Color(0xFF969FA2),
+                    fontSize: 14,
+                    fontWeight: FontWeight.w400),
+              ),
+              const SignupButton(),
+              SizedBox(
+                height: Get.height * 0.08,
+              ),
+              const ETextButton()
+            ],
+          ),
         ),
       ),
     );
--- test/module/login/view/login_view3_test.dart
+++ test/module/login/view/login_view3_test.dart
@@ -1,5 +1,101 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_slicing_project/module/login/view/login_view3.dart';
+import 'package:flutter_slicing_project/state_util.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  testWidgets('login view3 ...', (tester) async {});
+  Widget widgetUnderTest() {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.green)
+            .copyWith(background: Colors.white),
+      ),
+      home: const LoginView3(),
+      navigatorKey: Get.navigatorKey,
+    );
+  }
+
+  group(
+    'Widget testing for ELogin3:',
+    () {
+      testWidgets(
+        'Ensuring the Icon widget for logo is appearing',
+        (widgetTester) async {
+          await widgetTester.pumpWidget(widgetUnderTest());
+          expect(find.byType(Image), findsOneWidget);
+        },
+      );
+      testWidgets(
+        'Ensuring there is Text widget "Biggest collection of 300+ layouts for iOS prototyping."',
+        (widgetTester) async {
+          await widgetTester.pumpWidget(widgetUnderTest());
+          expect(
+              find.text(
+                  'Biggest collection of 300+ layouts\nfor iOS prototyping.'),
+              findsOneWidget);
+        },
+      );
+      testWidgets(
+        'Ensuring there is Text widget "Login with social networks"',
+        (widgetTester) async {
+          await widgetTester.pumpWidget(widgetUnderTest());
+          expect(find.text('Login with social networks'), findsOneWidget);
+        },
+      );
+      testWidgets(
+        'Ensuring there are Login with Facebook and Twitter button',
+        (widgetTester) async {
+          await widgetTester.pumpWidget(widgetUnderTest());
+
+          final loginWFacebook = find.ancestor(
+            of: find.text('Facebook'),
+            matching: find.byType(ElevatedButton),
+          );
+
+          final loginWTwitter = find.ancestor(
+            of: find.text('Twitter'),
+            matching: find.byType(ElevatedButton),
+          );
+
+          expect(loginWFacebook, findsOneWidget);
+          expect(loginWTwitter, findsOneWidget);
+          // expect(find.byType(IconData), findsNWidgets(2));
+        },
+      );
+      testWidgets(
+        'Ensuring there is Text widget "or sign up with Email"',
+        (widgetTester) async {
+          await widgetTester.pumpWidget(widgetUnderTest());
+          expect(find.text('or sign up with Email'), findsOneWidget);
+        },
+      );
+      testWidgets(
+        'Ensuring there is Signup with Email button',
+        (widgetTester) async {
+          await widgetTester.pumpWidget(widgetUnderTest());
+
+          final signupWEmail = find.ancestor(
+            of: find.text('Sign Up'),
+            matching: find.byType(ElevatedButton),
+          );
+
+          expect(signupWEmail, findsOneWidget);
+        },
+      );
+      testWidgets(
+        'Ensuring there is a Text Login with Email button',
+        (widgetTester) async {
+          await widgetTester.pumpWidget(widgetUnderTest());
+
+          final loginWEmail = find.ancestor(
+            of: find.text('Login with Email'),
+            matching: find.byType(TextButton),
+          );
+
+          expect(loginWEmail, findsOneWidget);
+        },
+      );
+    },
+  );
 }
